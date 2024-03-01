f3353829397e58601e0a275cb685817b579b18e6
Ebby Chakkungal <121340114+EbbyChack@users.noreply.github.com>
2024-03-01 16:23:06 +0100

    ok

--- Models/Angrafica.cs
+++ Models/Angrafica.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,6 +16,8 @@ namespace _1MarzoTest.Models
         public string Citta { get; set; }
         public string Cap { get; set; }
         public string CF { get; set; }
+
+        [Display(Name = "Id Violazione")]
         public int IdViolazione { get; set; }
 
     }
